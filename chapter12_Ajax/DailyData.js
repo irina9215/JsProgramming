6a2d0993558d6f051bce8c4d1f89810898e1be31
irina9215 <moyanxian15@163.com>
2015-01-27 09:37:01 +0800

    Update DailyData.js

--- chapter12_Ajax/DailyData.js
+++ chapter12_Ajax/DailyData.js
@@ -1,6 +1,5 @@
 $(function (){
   getAirDayData();
-  getLast7DaysAQI();
 });
 
 function getAirDayData() {
@@ -13,12 +12,19 @@ function getAirDayData() {
     cache:false,
     error:function () { alert(“提示：获取数据失败”)；}，
     success：function(result) {
+      
       var length = result.Head.length;
+      
       if(length != 0 ) {
         var cityData = result.Head[length-1];
         genDashboardChart(cityData.AQI_24H);
         genCityAQIShow(cityData);
         
+        var data = NewData(result.Head[0].Rec_Time.split(' ')[0]);
+        date = date.getFullYear() + '月' + (date.getMonth() + 1) + '月' + date.getDate() + '日';
+        
+         $('#lblDate1').html(date);
+         $('#lblDate2').html('（' + date + '）');
       }
     }
     
